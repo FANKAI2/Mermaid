7de8ba252f3709373aadb2586b1adc17b869d005
KLTEA <sun3203021228@gmail.com>
2024-10-21 21:15:54 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.HighNotes, function (sprite, oth
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.LowNotes, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.spray, 200)
-    music.play(music.createSong(hex`003c000408020106001c00010a006400f40164000004000000000000000000000000000000000234000000080002061108001000010610001800020812180020000108200028000206112800300001063000380002080f380040000108`), music.PlaybackMode.LoopingInBackground)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.MiddleNotes, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.spray, 200)
@@ -90,5 +89,12 @@ let HighNote = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.HighNotes)
 tiles.placeOnTile(HighNote, tiles.getTileLocation(12, 10))
+music.play(music.createSong(hex`003c000408020106001c00010a006400f40164000004000000000000000000000000000000000239000000080002061208001000020d121000180002081418002000020f1420002800020a162800300002111630003800020a1638004000030a1116`), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSong(hex`003c000408020103001c0001dc00690000045e01000400000000000000000000056400010400032a0000000800012508000c00012c0c0010000129100018000127200028000125280030000129380040000127`), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSong(hex`003c000408020106001c00010a006400f40164000004000000000000000000000000000000000234000000080002061108001000010610001800020812180020000108200028000206112800300001063000380002080f380040000108`), music.PlaybackMode.LoopingInBackground)
+pauseUntil(() => true)
+if (true) {
+	
+}
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
